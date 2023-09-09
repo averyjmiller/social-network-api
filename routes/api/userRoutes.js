e71f0a3f68999a6cfca455a46439925bb2f70252
Avery Miller <averymiller2060@gmail.com>
2023-09-09 17:37:56 -0400

    implemented CRUD user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,4 +9,10 @@ const {
   removeUserFriend
 } = require('../../controllers/userController');
 
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addUserFriend).delete(removeUserFriend);
+
 module.exports = router;
